f15df6c3ceba9e6aab998e405d4e64086030b608
Neon-jeff <omaksteejay@gmail.com>
2023-09-14 17:26:01 +0100

    Static build Change

--- core/settings.py
+++ core/settings.py
@@ -136,15 +136,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+COMPRESS_ROOT = BASE_DIR /'static'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URLS ='/media/'
+MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR,'static/'),
-   os.path.join(BASE_DIR,'staticfiles/'),
+   os.path.join(BASE_DIR,'static/')
 ]
 
 
