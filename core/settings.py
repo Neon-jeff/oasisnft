90a6344817f8a2e37d98358f6b0eb9bf99744321
Neon-jeff <omaksteejay@gmail.com>
2023-09-14 19:36:49 +0100

    DB change

--- core/settings.py
+++ core/settings.py
@@ -94,10 +94,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':env('DBUSER'),
+        'PASSWORD':env('DBPW'),
+        'HOST':env('DBHOST'),
+        'PORT':env('DBPORT')
     }
 }
 
@@ -150,6 +161,15 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR,'static')
 ]
 
+CSRF_USE_SESSIONS=True
+
+CSRF_COOKIE_SECURE=False
+
+SESSION_COOKIE_SECURE=False
+
+CSRF_TRUSTED_ORIGINS=[
+   "https://oasisnft-production.up.railway.app/"
+]
 
 
 COMPRESS_ENABLED = True
