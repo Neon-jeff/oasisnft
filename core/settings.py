9616ede3c602c0b7beab0a0c1f663a00320dd2e3
Neon-jeff <omaksteejay@gmail.com>
2023-10-05 21:09:08 +0100

    Fixed user search

--- core/settings.py
+++ core/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://nelloarts.com']
 
 # cloudinary config
 cloudinary.config(
